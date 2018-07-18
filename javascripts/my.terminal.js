0bd73903f169fedfd6a5959b34f8fbcd944e7fd0
Yuriy Chumak <yuriy.chumak@mail.com>
2018-07-19 01:34:39 +0300

    webgl fix - no clearing window context between switches

--- javascripts/my.terminal.js
+++ javascripts/my.terminal.js
@@ -158,6 +158,8 @@ var Module = {
    canvas: (function() {
       var canvas = document.getElementById('canvas');
 
+      // please, add to glutCreateContext() 'preserveDrawingBuffer: true' attribute!
+
       // As a default initial behavior, pop up an alert when webgl context is lost. To make your
       // application robust, you may want to override this behavior before shipping!
       // See http://www.khronos.org/registry/webgl/specs/latest/1.0/#5.15.2
